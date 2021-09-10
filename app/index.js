f59b843452d3e51cc778281325da1e93775b9148
Robert Crocker <robert@vizsimply.com>
2021-09-10 01:41:08 -0300

    Successfully uninstall survey-react and updated webpack to use scss.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
+import "./index.scss"
 
 // Components
 import MyForm from "./components/MyForm"
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
