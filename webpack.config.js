2a8cdbba71f10fecc7e11b6c47dd8d0489924e27
Robert Crocker <robert@vizsimply.com>
2021-09-09 23:08:50 -0300

    Successfully set up webpack and rendered hello world.

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,20 @@ const { webpack } = require("webpack")
 
 module.exports = {
   entry: "./app/index.js",
-  module: {
-    rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.(js)$/, use: "babel-loader" },
-    ],
-  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "app/index.html",
+    }),
+  ],
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
 }
