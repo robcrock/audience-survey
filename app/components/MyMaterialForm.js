8a3f4122aef4c67c4da29a50c4795ebe01bb00ad
Robert Crocker <robert@vizsimply.com>
2021-09-11 14:45:18 -0300

    Completed the structure of the questions.

--- app/components/MyMaterialForm.js
+++ app/components/MyMaterialForm.js
@@ -19,22 +19,145 @@ function MyMaterialForm() {
 
   return (
     <FormControl component="fieldset">
-      <FormLabel component="legend">Gender</FormLabel>
+      <FormLabel component="legend">
+        What is the Seniority Level of this audience?
+      </FormLabel>
       <RadioGroup
-        aria-label="gender"
-        name="gender1"
-        value="gender"
+        aria-label="senoriority"
+        name="senority"
+        value=""
         onChange={handleChange}
       >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel value="all" control={<Radio />} label="All" />
+        <FormControlLabel value="high" control={<Radio />} label="High" />
+        <FormControlLabel value="medium" control={<Radio />} label="Medium" />
+        <FormControlLabel value="low" control={<Radio />} label="Low" />
+      </RadioGroup>
+      <FormLabel component="legend">
+        If this audience works in a certian Division, what is that Division?
+      </FormLabel>
+      <RadioGroup
+        aria-label="division"
+        name="division"
+        value=""
+        onChange={handleChange}
+      >
         <FormControlLabel
-          value="disabled"
-          disabled
+          value="marketing"
           control={<Radio />}
-          label="(Disabled option)"
+          label="Marketing"
         />
+        <FormControlLabel value="finance" control={<Radio />} label="Finance" />
+        <FormControlLabel
+          value="management"
+          control={<Radio />}
+          label="Management"
+        />
+        <FormControlLabel value="hR" control={<Radio />} label="HR" />
+        <FormControlLabel
+          value="operations"
+          control={<Radio />}
+          label="Operations"
+        />
+      </RadioGroup>
+      <FormLabel component="legend">
+        Is the audience Internal or External
+      </FormLabel>
+      <RadioGroup
+        aria-label="internal-or-external"
+        name="internal-or-external"
+        value=""
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="internal"
+          control={<Radio />}
+          label="Internal"
+        />
+        <FormControlLabel
+          value="external"
+          control={<Radio />}
+          label="External"
+        />
+      </RadioGroup>
+      <FormLabel component="legend">
+        How familiar is the audience with the subject of this visualization?
+      </FormLabel>
+      <RadioGroup
+        aria-label="familiarity"
+        name="familiarity"
+        value=""
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="familiar"
+          control={<Radio />}
+          label="Familiar"
+        />
+        <FormControlLabel
+          value="unfamiliar"
+          control={<Radio />}
+          label="Unfamiliar"
+        />
+      </RadioGroup>
+      <FormLabel component="legend">
+        Does the audience expect the dashboard to be Interactive or Static?
+      </FormLabel>
+      <RadioGroup
+        aria-label="interactive-or-static"
+        name="interactive-or-static"
+        value=""
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="interactive"
+          control={<Radio />}
+          label="Interactive"
+        />
+        <FormControlLabel value="static" control={<Radio />} label="Static" />
+      </RadioGroup>
+      <FormLabel component="legend">
+        How much time does this audience have to spend with the dashboard?
+      </FormLabel>
+      <RadioGroup
+        aria-label="time"
+        name="time"
+        value=""
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="quick-glance"
+          control={<Radio />}
+          label="Quick Glance"
+        />
+        <FormControlLabel
+          value="in-depth-analysis"
+          control={<Radio />}
+          label="In-depth Analysis"
+        />
+        <FormControlLabel value="both" control={<Radio />} label="Both" />
+      </RadioGroup>
+      <FormLabel component="legend">
+        Will the audience be using this dashboard for Passive Monitoring or
+        Active Decision Making?
+      </FormLabel>
+      <RadioGroup
+        aria-label=""
+        name="passive-or-active"
+        value="passive-or-active"
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="passive-monitoring"
+          control={<Radio />}
+          label="Passive Monitoring"
+        />
+        <FormControlLabel
+          value="active-decision-making"
+          control={<Radio />}
+          label="Active Decision Making"
+        />
+        <FormControlLabel value="both" control={<Radio />} label="Both" />
       </RadioGroup>
     </FormControl>
   )
