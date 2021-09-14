b376190e200000ee91abbb08202f35f4d931c510
Robert Crocker <robert@vizsimply.com>
2021-09-13 22:44:04 -0300

    Cleaned up some commented code.

--- client/components/SurveyList.js
+++ client/components/SurveyList.js
@@ -31,17 +31,6 @@ const SurveyList = () => {
     } catch (err) {
       console.error(err.message)
     }
-    // fetch("http://localhost:5000/surveys", {
-    //   method: "DELETE",
-    //   body: JSON.stringify(data.formValues),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((response) => console.log("Success:", JSON.stringify(response)))
-    //   .catch((error) => console.error("Error:", error))
-    return
   }
 
   useEffect(() => {
